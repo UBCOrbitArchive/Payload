69d2b193c5da57dcaa423df8a541b9c36c05670a
Muchen He <mansurhe@outlook.com>
2017-02-18 19:24:46 -0800

    added arduino sketch for stepper motor control sys

---
+++ Servo Control Test/sketch_feb18a/sketch_feb18a.ino
@@ -0,0 +1,28 @@
+#include <Stepper.h>
+const int stepsPerRevolution = 200;
+const int motorSpeed = 50;
+const int stepLimit = 1000;
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+int stepCount = 0;
+bool forward = true;
+int increment;
+
+void setup() {}
+
+void loop() {
+    if (stepCount == 0) {
+        forward = true;
+    }else if (stepCount == stepLimit) {
+        forward = false;
+    }
+    if (forward) {
+        increment = stepsPerRevolution / 100;
+    }else{
+        increment = -stepsPerRevolution / 100;
+    }
+
+    myStepper.setSpeed(motorSpeed);
+    myStepper.step(increment);
+    stepCount = stepCount + increment;
+}
