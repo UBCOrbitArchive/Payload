5f0ffa6cd481e5ea79e606b2a3a951b9d8ed0135
Muchen He <mansurhe@outlook.com>
2017-02-19 23:36:00 -0800

    Removed useless code

--- Servo Control Test/ULN2003APC_Test/ULN2003APC_Test.ino
+++ Servo Control Test/ULN2003APC_Test/ULN2003APC_Test.ino
@@ -1,25 +1,18 @@
 #include <Stepper.h>
-const int stepsPerRevolution = 200;
-const int motorSpeed = 100;
+#define STEPS_PER_REV 32
+#define STEPS_PER_OUT 2048
+#define MOTOR_SPEED 500
 
-// initialize the stepper library on pins 8 through 11:
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
-bool forward = true;
-int increment;
+// stepper objs, param: IN1, IN2, IN3, IN4 (reverse order works - except reversed, duh)
+Stepper stepper(STEPS_PER_REV, 8, 10, 9, 11);
 
 void setup() {
-    myStepper.setSpeed(motorSpeed);
 }
 
+int  Steps2Take;
 void loop() {
-    // if (forward) increment = stepsPerRevolution / 100;
-    // else increment = -stepsPerRevolution / 100;
-    myStepper.step(stepsPerRevolution);
-    delay(500);
-    myStepper.step(-200);
-    delay(500);
-
-    // if (millis() % 5000 <= 10) {
-    //     forward = forward ? false : true;
-    // }
+    stepper.setSpeed(MOTOR_SPEED);
+    stepper.step(STEPS_PER_OUT);
+    stepper.setSpeed(MOTOR_SPEED);
+    stepper.step(-STEPS_PER_OUT);
 }
