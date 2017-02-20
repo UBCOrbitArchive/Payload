3084ce85f601b2d19615caaf0ae6b877b1c1037f
Muchen He <mansurhe@outlook.com>
2017-02-19 23:54:42 -0800

    Added potentiometer control

--- Servo Control Test/ULN2003APC_Test/ULN2003APC_Test.ino
+++ Servo Control Test/ULN2003APC_Test/ULN2003APC_Test.ino
@@ -3,16 +3,25 @@
 #define STEPS_PER_OUT 2048
 #define MOTOR_SPEED 500
 
+// pin assignments
+const int analogIn = A0;
+
 // stepper objs, param: IN1, IN2, IN3, IN4 (reverse order works - except reversed, duh)
 Stepper stepper(STEPS_PER_REV, 8, 10, 9, 11);
 
 void setup() {
+    stepper.setSpeed(MOTOR_SPEED);
+    Serial.begin(115200);
 }
 
-int  Steps2Take;
+double value;
 void loop() {
-    stepper.setSpeed(MOTOR_SPEED);
-    stepper.step(STEPS_PER_OUT);
-    stepper.setSpeed(MOTOR_SPEED);
-    stepper.step(-STEPS_PER_OUT);
+    // stepper.step(STEPS_PER_OUT);
+    // stepper.step(-STEPS_PER_OUT);
+    value = 1023 * (analogRead(analogIn) / 1023.0 - 0.5);
+    Serial.println(value);
+
+    stepper.setSpeed(abs(value));
+    if (value > 0) stepper.step(1);
+    else stepper.step(-1);
 }
